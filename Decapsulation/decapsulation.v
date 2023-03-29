6edfaf7e94c647699b6898416083cd9420b1ee4a
EnesErcin <ercine20@itu.edu.tr>
2023-03-29 16:50:09 +0300

    DEcapsulation -init

---
+++ Decapsulation/decapsulation.v
@@ -0,0 +1,198 @@
+`include "utils.vh"
+
+module ethernet_decapsulation 
+#(
+    parameter [47:0] destination_mac_addr  = 48'h023528fbdd66,
+    parameter [47:0] source_mac_addr       = 48'h072227acdb65
+)
+(
+    // Output Payload
+    output  reg     crc_err,adr_err,len_err, buffer_full,
+    output data_out_en,
+
+    // Ethernet RX Transmission ports
+    input [7:0]     gmii_data_in,
+    input           gmii_dv,gmii_er,gmii_en,clk,rst
+);
+
+// Dump waveforms with makefile
+`ifdef COCOTB_SIM
+initial begin
+    $dumpfile("sim.vcd");
+    $dumpvars(0,ethernet_decapsulation);
+end    
+`endif
+
+    //  Define Registers
+    reg         [3:0]                   state_reg = 0;                               // FSM State register, keep track of frame section 
+    reg         [8*10-1:0]              data_buf;                                    // Payload Buffer that holds data
+    reg         [13:0]                  byte_count = 0;                              // Track bytes in each state
+    reg         [31:0]                 crc_res     = {(`len_crc){1'b1}};             // Initate CRC, update with every processed byte
+    reg         [15:0]                 len_payload = 0;                              // Keep track of every Recived Payload Length
+    
+    wire                               cont_stages;
+    reg         [8*`len_addr-1:0]                  source_addr;
+    reg         [8*`len_addr-1:0]                  dest_addr;
+    reg         [8*`len_len-1:0]                    data_len;                       
+    reg         [8*`len_crc-1:0]                    data_crc;  
+
+    //  Ethernet Frame Encapsulation Stages
+    localparam  IDLE                = 4'd0,
+                PERMABLE            = 4'd1,
+                SDF                 = 4'd2,  
+                LEN                 = 4'd3,
+                PAYLOAD             = 4'd4,  
+                FCS                 = 4'd5,
+                EXT                 = 4'd6,
+                Dest_MAC            = 4'd7,
+                Source_Mac          = 4'd8,
+                Err                 = 4'd9;
+
+    localparam Permable_val = 8'b101010,                            
+               Start_Del_val= 8'b101011;                                            // IEEE defined Permable and Delimeter bytes
+
+
+    assign data_out_en = (state_reg == IDLE)? 1'b1:1'b0;
+    // Bufferred Data should not be transmitted while still receiving
+    assign cont_stages = (gmii_en && gmii_dv && !(gmii_er))? 1'b1:1'b0 ;
+
+    // Select the right output according to stages
+    always @(posedge clk) begin
+        case (state_reg)
+            IDLE        : data_buf    =   0;
+
+            PERMABLE    : data_buf    =   0;
+                            
+            SDF         : data_buf    =   0;
+
+            Dest_MAC    : dest_addr[(8*(`len_addr-byte_count)-1)+:8]         =    gmii_data_in; 
+                         
+            Source_Mac  : source_addr[(8*(`len_addr-byte_count)-1)+:8]       =    gmii_data_in;
+
+            LEN         : data_len[(8*(`len_addr-byte_count)-1)+:8]          =    gmii_data_in;
+
+            PAYLOAD     : data_buf[(8*(len_payload-byte_count)-1)+:8]        =   gmii_data_in;
+
+            EXT         : data_buf    =     0;
+
+            FCS         : data_crc[(8*(`len_crc-byte_count)-1)+:8]           =    gmii_data_in;
+        endcase
+    end
+
+    // Ethernet Frame Stages
+    always @(posedge clk) begin
+        if (!rst) begin
+            if (gmii_en) begin
+                    case (state_reg)
+                        IDLE:       begin
+                                        if (gmii_en && gmii_dv) begin
+                                            state_reg = PERMABLE;
+                                        end
+                                        else if (gmii_er) begin
+                                            state_reg =Err;
+                                        end
+                                    end 
+                        PERMABLE:   begin
+                                        if(byte_count < `len_perm-1 ) begin
+                                            byte_count  = byte_count +1;
+                                        end 
+                                        else begin
+                                            state_reg = SDF;
+                                            byte_count = 0;
+                                        end
+                                    end
+                        SDF:  begin
+                                        state_reg   = Dest_MAC;
+                                    end
+                        Dest_MAC:   begin
+                                        if (byte_count < `len_addr-1) begin
+                                            byte_count = byte_count + 1;
+                                        end 
+                                        else begin
+                                            byte_count= 0;
+                                            state_reg = Source_Mac;
+                                        end
+                                    end
+                        Source_Mac: begin
+                                        if (byte_count < `len_addr-1) begin
+                                            byte_count = byte_count + 1;
+                                        end 
+                                        else begin
+                                            byte_count= 0;
+                                            state_reg = LEN;
+                                        end  
+                                    end
+                        LEN:        begin
+                                        if (byte_count < `len_len-1) begin
+                                            byte_count = byte_count + 1;
+                                        end 
+                                        else begin
+                                            byte_count= 0;
+                                            state_reg = PAYLOAD;
+                                        end  
+                                    end
+                        PAYLOAD:    begin
+                                        if (byte_count < len_payload-1) begin
+                                            byte_count = byte_count + 1;
+                                            state_reg = PAYLOAD;
+                                        end 
+                                        else begin
+                                            byte_count= 0;
+                                            if (len_payload <= `min_payload_len)
+                                                state_reg = EXT;
+                                            else
+                                                state_reg = FCS;
+                                        end  
+                                    end
+
+                        EXT:        begin
+                                        if (byte_count < `min_payload_len-len_payload-1) begin
+                                            byte_count = byte_count + 1;
+                                        end 
+                                        else begin
+                                            state_reg = FCS;
+                                            byte_count = 0;
+                                        end
+                                    end
+
+                        FCS:        begin
+                                        if (byte_count < `len_crc-1) begin
+                                            byte_count = byte_count + 1;
+                                        end 
+                                        else begin
+                                            byte_count= 0;
+                                            state_reg = IDLE;
+                                        end 
+                                    end
+                        Err:        begin
+                            
+                                    end
+
+                        default: state_reg = IDLE;
+                    endcase
+            end
+        end 
+    end
+
+
+    //  Global Synchronous Reset
+    always @(posedge clk ) begin
+        if (rst) begin
+            len_payload                     = 0;
+            state_reg                       =IDLE;
+            data_buf                        = 0;
+            crc_res                         = {(`len_crc){1'b1}};
+            source_addr                     = 0;
+            dest_addr                       = 0;
+            data_len                        = 0;
+            data_crc                        = 0;
+            source_addr                     = 0;
+            dest_addr                       = 0;
+            data_len                        = 0;
+            data_crc                        = 0;
+        end
+    end
+
+endmodule
+
+
---
+++ Decapsulation/my_tests.py
@@ -0,0 +1,109 @@
+import cocotb 
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge ,RisingEdge, Timer
+import random
+
+async def reset(dut):
+    dut.rst.value   =   1
+    await(RisingEdge(dut.clk))
+    await   Timer(2,units="ns")
+    assert  (dut.state_reg.value.binstr == "0000") #  Reset did not restarted fsm
+    await   Timer(10,units="ns")
+    dut.rst.value = 0
+    await   Timer(10,units="ns")
+
+async def data_fill(dut,len):
+    assert(len <= 1500) # Maximum Frame Packet Must be 1500 Bytes !
+
+    if (len <46):
+        dut._log.info("Chosen package needs extension on frame. Len of extension : \t {}".format(46-len))
+    
+    await Timer(15,units="ns")
+    dut.buffer_empt.value = 0
+    dut.buffer_ready.value = 1
+    await(RisingEdge(dut.clk))
+
+    for i in range(0,len):
+        dut.data_in.value = random.randint(0,255)
+        dut.read_en.value = 1
+        await(RisingEdge(dut.clk))
+        dut.read_en.value = 0
+
+    await(RisingEdge(dut.clk))
+    dut.buffer_ready.value  = 0
+    dut.buffer_empt.value   = 1
+    await(RisingEdge(dut.clk))
+    await(RisingEdge(dut.clk))
+    assert(dut.buffer_data_valid.value.integer == 1)    # Data did not read completly 
+    assert(dut.len_payload.value.integer    ==  len)  # Data has not been written correct
+
+async def init_tx(dut,len_payload):
+    #   Length of frame sections
+    len_addr    = 6
+    len_len     = 2
+    len_crc     = 4
+    len_permable= 7
+    #
+    dut.en_tx.value = 0
+    clk = dut.clk
+    await RisingEdge(clk)
+    dut.rst.value   = 0
+    await RisingEdge(clk)
+    dut.en_tx.value = 1
+    await RisingEdge(clk)
+    assert (dut.state_reg.value.integer == 0)   #In wrong stage, should be in IDLE
+    await RisingEdge(clk)
+    assert (dut.state_reg.value.integer == 1)   #In wrong stage, should be in PERMABLE
+    await wait_multiple_clocks(clk,len_permable)
+    assert (dut.state_reg.value.integer == 2)   #In wrong stage, should be in SDF
+    await RisingEdge(clk)
+    assert (dut.state_reg.value.integer == 7)   #In wrong stage, should be in Dest_Mac
+    await wait_multiple_clocks(clk,len_addr)        
+    assert (dut.state_reg.value.integer == 8)   #In wrong stage, should be in Source_Mac
+    await wait_multiple_clocks(clk,len_addr)
+    assert (dut.state_reg.value.integer == 3)   #In wrong stage, should be in LEN
+    await wait_multiple_clocks(clk,len_len)        
+    assert (dut.state_reg.value.integer == 4)   #In wrong stage, should be in PAYLOAD
+    await wait_multiple_clocks(clk,len_payload)
+    if (len_payload < 46):
+        dut._log.info("Extenstion stage entered")
+        assert (dut.state_reg.value.integer == 6)   #In wrong stage, should be in EXT
+        await wait_multiple_clocks(clk,(46-len_payload))
+    assert (dut.state_reg.value.integer == 5)   #In wrong stage, should be in FCS
+    await wait_multiple_clocks(clk,len_crc)
+    assert (dut.state_reg.value.integer == 0)   #In wrong stage, should be in IDLE
+
+
+async def wait_multiple_clocks(clk,num):
+    for i in range (0,num):
+        await (RisingEdge(clk))
+
+        
+@cocotb.test()
+async def initate(dut):  
+    # Input signals of verilog module
+    #Control signals
+
+    clk = Clock(dut.clk, 2, 'ns')
+    cocotb.start_soon(clk.start())  #    Initiate Clock
+    await   Timer(10,units="ns")
+    await  reset(dut)
+    await   Timer(10,units="ns")
+    await data_fill(dut,16)
+
+
+@cocotb.test()
+async def transmit(dut):  
+    # Input signals of verilog module
+    #Control signals
+
+    clk = Clock(dut.clk, 2, 'ns')
+    cocotb.start_soon(clk.start())  #    Initiate Clock
+    await   Timer(10,units="ns")
+    await  reset(dut)
+    await   Timer(10,units="ns")
+    len_payload = 50
+    await data_fill(dut,len_payload )
+    await Timer(45,units="ns")
+    await init_tx(dut,len_payload)
+
