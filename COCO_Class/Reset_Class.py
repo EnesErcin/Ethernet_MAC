f89dbbdde19f54bc880b0c65da8a82b8135b64cf
EnesErcin <ercine20@itu.edu.tr>
2023-05-26 16:54:48 +0300

    Working on tx-rx test bench configuration

--- COCO_Class/Reset_Class.py
+++ COCO_Class/Reset_Class.py
@@ -2,12 +2,30 @@ import cocotb
 from cocotb.triggers import RisingEdge, FallingEdge
 
 class Reset:
-    def _init_reset(self,reset_signal,active_reset):
+    def _init_reset(self,reset_signal =None,active_reset=True):
+        ####
+        # - active level = True (nrst)
+        #
+        ####
         self.local_reset = False
         self.ext_reset = False
         self.rst_state = True
 
+        if active_reset is not None:
+            cocotb.start_soon(self._run_reset(reset_signal,bool(active_reset)))
+
+        self._update_Reset()
     
+    ## !!
+    def assert_reset(self, val=None):
+        if val is None:
+            self.assert_reset(True)
+            self.assert_reset(False)
+        else:
+            self._local_reset = bool(val)
+            self._update_reset()
+    ## !!
+
     def _update_Reset(self):
         new_rst_state = self.ext_reset or self.local_reset
 
@@ -15,6 +33,15 @@ class Reset:
             self.rst_state = new_rst_state
             self.__handle__reset(new_rst_state)
 
-    def __handle__reset(self):
+    def __handle__reset(self,state):
         pass
-        
+    
+    async def _run_reset(self,reset_signal,active_level):
+        while True:
+            if (bool(reset_signal)):
+                await FallingEdge(reset_signal)
+                self._update_Reset()
+            else:
+                await RisingEdge(reset_signal)
+                self._update_Reset()
+
